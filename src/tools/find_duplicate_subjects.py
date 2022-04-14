e9607045684fa9950e51ca6c02d417ccff99ffe4
Yiftach Edelstain <Yiftach.edelstain@gmail.com>
2022-04-15 01:21:50 +0300

    finished tool iterating folders

--- src/tools/find_duplicate_subjects.py
+++ src/tools/find_duplicate_subjects.py
@@ -1,6 +1,32 @@
 import argparse
 import os 
 
+def iterate_pathology_structure(path):
+    try:
+        path = str(path, 'utf-8')
+    except:
+        pass
+
+    if str(path).endswith('wav'):
+        return path.split('/')[-1].split('-')[0]
+    children = [os.path.join(path,child) for child in os.listdir(path) if child!='.DS_Store']
+    children = [iterate_pathology_structure(child) for child in children]
+    if len(children)== 0:
+        return children
+    if isinstance(children[0],str):
+        children = list(set(children))
+        pathology = path.split('/')[-1]
+        return [(child,pathology) for child in children]
+    children = [child for sublist in children for child in sublist]
+    return children
+
+def consolidate_subjects(subjects):
+    subj_dict = {}
+    for (subject,pathology) in subjects:
+        if subject not in subj_dict:
+            subj_dict[subject]=[]
+        subj_dict[subject] += [pathology]
+    return subj_dict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -9,13 +35,8 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     directory = os.fsencode(args.path)
-    
-    for file in os.listdir(directory):
-        assert file in [b"male",b"female"], f"structure should follow with genders"
-        path = os.path.join(directory,file)
-        for pathology in os.listdir(path):
-            sub_path = os.path.join(path,pathology)
-            print(f"{pathology}")
-            for sub_pathology in os.listdir(sub_path):
-                print(f"    {sub_pathology}")
-                
+    subjects = iterate_pathology_structure(directory)
+    subj_dict = consolidate_subjects(subjects)
+    count = len([(subj,patho) for (subj,patho) in subj_dict.items() if len(patho)>1])
+    print(len(subj_dict.items()))
+    print(count)
