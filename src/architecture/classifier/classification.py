8dabcc9f7a4075e32ec4ae3d0c6c074a6de7e41b
Yiftach Edelstain <yiftachedelstain@Yiftachs-MacBook-Pro.local>
2022-05-13 02:58:44 +0300

    add batchnorm and refactor ordering

--- src/architecture/classifier/classification.py
+++ src/architecture/classifier/classification.py
@@ -6,15 +6,17 @@ import torch.nn as nn
 class Classifier(nn.Module):
     def __init__(self,dimensions=[],out_dim=1) -> None:
         super().__init__()
-        self.backend = yamnet(pretrained=True,remove_classification_layer=True)
         layers = []
         input_dim=1024
         for dimension in dimensions:
             layers += [nn.Linear(input_dim,dimension,bias=False),
+                        nn.BatchNorm1d(num_features=dimension),
                         nn.ReLU()]
             input_dim = dimension
         layers+=[nn.Linear(input_dim,out_dim,bias=False)]
-        self.classification = nn.Sequential(*layers)
+      
+        self.backend = yamnet(pretrained=True,remove_classification_layer=True)
+        self.classification = nn.Sequential(self.backend,*layers)
 
     def forward(self,x):
-        return self.classification(self.backend(x))
+        return self.classification(x)
