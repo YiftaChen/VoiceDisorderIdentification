added augmentation and fixed reporting validation acc, added precision+Recall+loss to board

src/datasets/SvdExDataset.py
@@ -4,11 +4,11 @@ import torch
 from torch.utils.data import Dataset
 from scipy.io import wavfile
 import torch.nn as nn
-from transformations import PadWhiteNoise,ToTensor,Truncate,ToOneHot,WaveformToInput
+from transformations import RandomFlip,PadWhiteNoise,ToTensor,Truncate,ToOneHot,WaveformToInput
 import librosa
 from core.params import CommonParams as cfg,PathologiesToIndex
 
-default_transforms = nn.Sequential(PadWhiteNoise(),ToTensor(),Truncate(cfg.SVD_SAMPLE_RATE*cfg.VOICE_SAMPLE_MIN_LENGTH),WaveformToInput())
+default_transforms = nn.Sequential(RandomFlip(), PadWhiteNoise(),ToTensor(),Truncate(60000),WaveformToInput())
 default_label_transforms = nn.Sequential(ToOneHot())
 
 class SvdExtendedVoiceDataset(Dataset):
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     from tqdm import tqdm
     from torch.utils.data import DataLoader
 
-    dataset = SvdExtendedVoiceDataset(r"/Users/yiftachedelstain/Development/VoiceDisorderIdentification/data/SVD - Extended/",classification_binary=True)
+    dataset = SvdExtendedVoiceDataset(r"/home/yiftach.ede@staff.technion.ac.il/data/SVD",classification_binary=True)
     loader = DataLoader(
         dataset,
         batch_size=128,

src/tools/apply_augmentation.py
@@ -3,7 +3,7 @@ from importlib.resources import path
 import os 
 import json 
 import collections
-from isort import file
+# from isort import file
 # from pyrsistent import T
 from scipy.io import wavfile
 import scipy.io
@@ -23,12 +23,12 @@ def sf_load_from_int16(fname):
 def iterate_files(path):
     children = []
     for root, _ , files in tqdm(os.walk(path)):
-        files = list([f for f in files])
-        files = [os.path.join(root,f) for f in files if f.endswith('wav') and not f.endswith('reversed.wav')]
-        waveforms_files = [(f,sf_load_from_int16(f)) for f in files]
+        files = list([f for f in files if f.endswith('wav') and not f.endswith('reversed.wav') and  not f.startswith('.')])
+        # files = [os.path.join(root,f) for f in files ]
+        waveforms_files = [(f,sf_load_from_int16(os.path.join(root,f))) for f in files]
         for f,(waveform,sr) in waveforms_files:
-            filename = f.split('.')[0]
-            sf.write(f"{filename}_reversed.wav",waveform[::-1],sr)
+            filename = os.path.join(root,f"{f.split('.')[0]}_reversed.wav")
+            sf.write(f"{filename}",waveform[::-1],sr)
         # children+=[os.path.join(root,f) for f in files if f.endswith('wav')]
     # return children
 
@@ -37,4 +37,4 @@ def consolidate_files(children):
     return list(children.values())
 
 if __name__=="__main__":
-    print(iterate_files("/Users/yiftachedelstain/Development/VoiceDisorderIdentification/data/SVD - Extended"))
+    print(iterate_files("/home/yiftach.ede@staff.technion.ac.il/data/SVD"))

src/trainer/configurableTrain.py
@@ -10,14 +10,14 @@ from architecture.backend.yamnet.model import yamnet_category_metadata
 
 from architecture.classifier.classification import Classifier
 
-from datasets.SvdExDataset import SvdCutOffShort
+from datasets.SvdExDataset import SvdCutOffShort,SvdExtendedVoiceDataset
 import trainer.train_svd as svd_trainer
 import torch.nn as nn
 import torch.optim 
 from ray import tune
 import pickle
 
-dataset = SvdCutOffShort(r"/home/chenka@staff.technion.ac.il/Desktop/SVD",classification_binary=True,overfit_test = False)
+dataset = SvdExtendedVoiceDataset(r"/home/yiftach.ede@staff.technion.ac.il/data/SVD",classification_binary=True)
 
 def train_model(config):
     print(f'test config: {config}')

src/trainer/train_svd.py
@@ -5,6 +5,7 @@ import math
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from ray import tune
+import numpy as np
 
 class Trainer(object):
     def __init__(self,dataset,model,optimizers,critereon,hyper_params,early_stop=float('inf'),device=None,verbose=False) -> None:
@@ -66,6 +67,7 @@ class Trainer(object):
                 epoch_accuracies = []
                 epoch_precisions = []
                 epoch_recalls = []
+                epoch_losses = []
                 with tqdm(self.train_set,disable=self.disableTQDM) as pbar:
                     for idx,sample in enumerate(pbar):                
                         x = sample['data'].to(device=self.device)
@@ -89,12 +91,13 @@ class Trainer(object):
                         epoch_accuracies += [accuracy]
                         epoch_precisions += [precision]
                         epoch_recalls += [recall]
-
+                        epoch_losses += [loss]
                         pbar.set_description(f"train epoch {epoch}, train loss is {loss.item()}, Accuracy {accuracy*100}%, Precision {precision*100}%, Recall {recall*100}%")
                         if idx == len(self.train_set)-1 :
                             epoch_accuracy = sum(epoch_accuracies)/len(epoch_accuracies)
                             epoch_precision = sum(epoch_precisions)/len(epoch_precisions)
                             epoch_recall = sum(epoch_recalls)/len(epoch_recalls)
+                            epoch_loss = sum(epoch_losses)/len(epoch_losses)
                             pbar.set_description(f"train epoch {epoch}, train loss:{running_loss} , Mean Accuracy:{epoch_accuracy*100}%, Mean Precision:{epoch_precision*100}%, Mean Recall:{epoch_recall*100}%")
 
                         # self.writer.add_scalar('Loss/train',loss.item(),idx)
@@ -121,9 +124,10 @@ class Trainer(object):
                                 accuracy = torch.sum(predictions==y)/len_predictions             
                                 precision = torch.sum(predictions*(predictions==y))/torch.sum(predictions)
                                 recall = torch.sum(predictions*(predictions==y))/torch.sum(y)
-                                vald_accuracies += [accuracy]
-                                vald_precisions += [precision]
-                                vald_recalls += [recall]
+                                vald_accuracies += [accuracy.cpu().item()]
+                                vald_precisions += [precision.cpu().item()]
+                                vald_recalls += [recall.cpu().item()]
+                                vald_losses += [loss.cpu().item()]
                             # self.writer.add_scalar('Loss/validation',loss.item(),idx)
                             # self.writer.add_scalar('Accuracy/validation',accuracy,idx)
                             # self.writer.add_scalar('Precision/validation',precision,idx)
@@ -131,8 +135,14 @@ class Trainer(object):
                             
                             t.set_description(f"validation epoch {epoch}, validation loss is {loss.item()}, Accuracy {accuracy*100}%, Precision {precision*100}%, Recall {recall*100}%")            
 
-                
-                tune.report(valid_acc=accuracy.item(),train_acc=epoch_accuracy.item())                        
+                accuracy = np.array(vald_accuracies).mean()
+                precision = np.array(vald_precisions).mean()
+                recall = np.array(vald_recalls).mean()
+                loss = np.array(vald_losses).mean()
+                tune.report(valid_acc=accuracy,train_acc=epoch_accuracy.item())                        
+                tune.report(valid_precision=precision,train_precision=epoch_precision.item())                        
+                tune.report(valid_recall=recall,train_recall=epoch_recall.item())                        
+                tune.report(valid_loss=loss,train_loss=epoch_loss.item())                        
 
                 if (accuracy.item()>last_acc):
                     last_acc=accuracy.item()

src/transformations/transform.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import numpy as np
 import torchaudio.transforms as ta_trans
 from core.params import CommonParams, YAMNetParams
+import random
 
 class ToOneHot(nn.Module):
     def __call__(self, classification):
@@ -18,16 +19,37 @@ class ToTensor(nn.Module):
 
     def __call__(self, sample):
         return torch.from_numpy(sample).float()
+
+class RandomFlip(nn.Module):
+    """Randomly flips the sample"""
+    def __init__(self,probability=0.5):
+        self.probability=probability
+
+    def __call__(self,sample):
+        if random.random() < self.probability:
+            return np.array(sample[::-1])
+        return sample
+        
+# class RandomFlip(nn.Module):
+#     """Randomly flips the sample"""
+#     def __init__(probability=0.5):
+#         self.probability=probability
+
+#     def __call__(self,sample):
+#         if random.random() < self.probability:
+#             return sample[::-1]
+      
+
 class PadWhiteNoise(nn.Module):
-    """Convert ndarrays in sample to Tensors."""
+    """Pads white noise to short audio samples."""
 
     def __call__(self,sample,sr=50000):
-        if len(sample)>50000:
+        if len(sample)>60000:
             return sample
         
         mean = sample.mean()
         variance = sample.var()
-        noise = np.random.normal(mean,variance,sr-len(sample))/10
+        noise = np.random.normal(mean,variance,60000-len(sample))/10
         signal=np.concatenate((sample,noise))
 
         return signal